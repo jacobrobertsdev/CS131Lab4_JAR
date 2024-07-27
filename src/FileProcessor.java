dcd50d0625b4f7023a4623cb2eabd70cbe19494c
Jacob Roberts <jacobroberts.dev@gmail.com>
2024-07-27 10:49:36 -0500

    Initial commit

---
+++ src/FileProcessor.java
@@ -0,0 +1,23 @@
+
+
+public class FileProcessor {
+
+	private String fileName;
+	private int stringLength;
+	private stringList ArrayList<String>;
+	private Scanner input;
+	
+	public FileProcessor(String fileName, int stringLength) {
+		
+			
+	}//end empty-argument constructor
+	
+	public int getArrayListSize() {
+		
+	}//end getArrayListSize
+	
+	public void processFile() {
+		
+		
+	}//end processFile
+}//end class
