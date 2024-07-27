630e5265355c58cfc44dfab05460e784db7004cf
Jacob Roberts <148710064+jacobrobertsdev@users.noreply.github.com>
2024-07-27 14:43:58 -0500

    Update BadString.java (fix indentation)

--- src/BadString.java
+++ src/BadString.java
@@ -3,23 +3,23 @@ public class BadString{
   
   public static void main(String[] args) {
     	
-	  	// Test that processor works
-	  	// Create FileProcessor
+	// Test that processor works
+	// Create FileProcessor
     	FileProcessor goodList = new FileProcessor("GoodString.txt",10);
     	
     	// Process Input
-		goodList.processFile();
+	goodList.processFile();
 		
-		// Check length of ArrayList
-		System.out.println(goodList.getArrayListSize());
+	// Check length of ArrayList
+	System.out.println(goodList.getArrayListSize());
 		
-		// Print contents of ArrayList
-		System.out.println(goodList.getStringList());
+	// Print contents of ArrayList
+	System.out.println(goodList.getStringList());
 		
-		System.out.println();
+	System.out.println();
 		
-		// Test exception handling (should throw "String is too long!")
-		// Create FileProcessor
+	// Test exception handling (should throw "String is too long!")
+	// Create FileProcessor
     	FileProcessor badList = new FileProcessor("BadString.txt",10);
 
     	// Process Input
